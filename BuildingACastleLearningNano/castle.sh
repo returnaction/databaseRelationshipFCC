b46f21dd4fac29e5693c26709397fd91408c355e
returnaction <obergannv@gmail.com>
2024-01-03 12:39:26 -0500

    Nano Building a castle is completed

---
+++ BuildingACastleLearningNano/castle.sh
@@ -0,0 +1,22 @@
+echo "
+Welcome to my castle
+"
+
+echo "
+ /\                /\  
+/  \              /  \  
+|  |______________|  | 
+|                    |
+|   []    []    []   |
+|                    |
+|         __         |
+|________|  |________|
+"
+
+# nano file_name 			create a file using nano
+
+# CTL + o   			writght down to nano
+# CTL + x 			exit nano
+# CTL + k				cut the text
+
+	
